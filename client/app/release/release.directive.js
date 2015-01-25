8e58ec25ae8e882264039ccfb7250956ace6fceb
Zoe Landon <zoe@leporidaemedia.com>
2015-01-24 22:20:16 -0800

    cards flip on hover

--- client/app/release/release.directive.js
+++ client/app/release/release.directive.js
@@ -5,16 +5,9 @@ angular.module('rcrdlistApp')
     return {
       templateUrl: 'app/release/release.html',
       restrict: 'EA',
-      controller: function($scope, $mdDialog) {
+      controller: function($scope) {
         $scope.showAbout = function(ev) {
-          $mdDialog.show(
-            $mdDialog.alert()
-              .title('This is an alert title')
-              .content('You can specify some description text in here.')
-              .ariaLabel('Password notification')
-              .ok('Got it!')
-              .targetEvent(ev)
-          );
+
         };
         
         $scope.playAlbum = function() {
