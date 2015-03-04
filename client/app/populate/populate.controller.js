658bfb6568b46ad2c4314e0fcacb06cb3bdfa502
Zoe Landon <zoe@leporidaemedia.com>
2015-03-03 22:31:07 -0800

    couldn't get to any real code today :x

--- client/app/populate/populate.controller.js
+++ client/app/populate/populate.controller.js
@@ -4,4 +4,10 @@ angular.module('rcrdlistApp')
   .controller('PopulateCtrl', function ($scope, $http) {
     $http.get('/api/populate').success(function() {
     });
+  
+    $scope.submit = function() {
+      var entry = null;
+      $http.post('/api/populate', entry).success(function() {
+      });
+    };
   });
