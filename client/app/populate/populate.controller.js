fec204c42a9da3e4cf018994aeccd2a203c95599
Zoe Landon <zoe@leporidaemedia.com>
2015-03-04 22:43:34 -0800

    entry definition

--- client/app/populate/populate.controller.js
+++ client/app/populate/populate.controller.js
@@ -6,7 +6,28 @@ angular.module('rcrdlistApp')
     });
   
     $scope.submit = function() {
-      var entry = null;
+      var entry = {artist: $scope.artist,
+                   album: $scope.album,
+                   summary: $scope.summary,
+                   genre: $scope.genre,
+                   bandcamp: $scope.bandcamp,
+                   spotify: $scope.spotify,
+                   jamendo: $scope.jamendo,
+                   soundcloud: $scope.soundcloud,
+                   amazon: $scope.amazon,
+                   itunes: $scope.itunes,
+                   gplay: $scope.gplay,
+                   twitter: $scope.twitter,
+                   facebook: $scope.facebook,
+                   youtube: $scope.youtube,
+                   gplus: $scope.gplus,
+                   scSocial: $scope.scSocial,
+                   picker: $scope.picker,
+                   bcID: $scope.bcID,
+                   scID: $scope.scID,
+                   jmID: $scope.jmID,
+                   spID: $scope.spID
+                  };
       $http.post('/api/populate', entry).success(function() {
       });
     };
