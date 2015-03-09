cbd9526dd5a6d9824ed678afcfaa4379b2699186
Zoe Landon <zoe@leporidaemedia.com>
2015-03-09 07:22:10 -0700

    added About page

---
+++ client/app/about/about.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('rcrdlistApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,15 +1,5 @@
 'use strict';
 
 angular.module('rcrdlistApp')
-  .controller('NavbarCtrl', function ($scope, $location) {
-    $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
-    }];
-
-    $scope.open = false;
-
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
+  .controller('NavbarCtrl', function () {
   });
